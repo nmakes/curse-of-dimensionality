08f6c201ada80cdcca80ac9e87100688ed673a15
nmakes <nav.naveenvenkat@gmail.com>
2018-08-30 23:11:13 +0530

    adding lpnorm tests

---
+++ lpnorm.py
@@ -0,0 +1,12 @@
+'''
+    Written by Naveen Venkat
+    Final Year Undergraduate Student, Dept. of CSIS
+    Birla Institute of Technology and Science, Pilani
+    www.naveenvenkat.com | nav.naveenvenkat@gmail.com
+'''
+
+import data
+
+def runLpNormComparison(n, d, plist):
+
+    
