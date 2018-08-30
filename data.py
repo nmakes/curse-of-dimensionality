d6b38e9562a5ebcd38ab647a88dbe70adcdef813
nmakes <nav.naveenvenkat@gmail.com>
2018-08-30 17:54:13 +0530

    adding data processing

---
+++ data.py
@@ -0,0 +1,16 @@
+'''
+    Written by Naveen Venkat
+    Final Year Undergraduate Student, Dept. of CSIS
+    Birla Institute of Technology and Science, Pilani
+    www.naveenvenkat.com | nav.naveenvenkat@gmail.com
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def getRandomData(n, d):
+    # n data points of d dimensions
+    return np.random.rand(n,d)
+
+def getSim(x1, x2):
+    return np.dot(x1, x2)
